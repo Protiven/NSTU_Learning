5547092228721a4528557542fa7b36179a62e8f9
Protiven <39469340+Protiven@users.noreply.github.com>
2019-10-27 17:34:22 +1200

    КС 3

--- Компьютерные сети/Лаба3/CLIENT/CLIENT/Form1.cs
+++ Компьютерные сети/Лаба3/CLIENT/CLIENT/Form1.cs
@@ -1,4 +1,8 @@
 ﻿using System;
+using System.Threading;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,9 +16,115 @@ namespace CLIENT
 {
     public partial class Form1 : Form
     {
+        static private Socket Client;
+        private string ip = null;
+        private int port = 0;
+        private string login = "";
+        private Thread th_add;
         public Form1()
         {
             InitializeComponent();
+
+            richTextBox1.Enabled = false;
+            richTextBox2.Enabled = false;
+            textBox1.Enabled = false;
+            button1.Enabled = false;
+            button2.Enabled = false;
+        }
+
+ 
+
+        private void button2_Click(object sender, EventArgs e) // Отправка сообщения
+        {
+            string message = richTextBox2.Text;
+            if (message != "")
+                sendMessage(message);
+        }
+
+        private void button1_Click(object sender, EventArgs e)  // Вставка логина
+        {
+            login = textBox1.Text;
+            if (login != "" && login!= " ")
+            {
+                richTextBox2.Enabled = true;
+                button2.Enabled = true;
+                textBox1.Enabled = false;
+                button1.Enabled = false;
+            }
+        }
+
+        private void sendMessage(string message)
+        {
+            if (message != " " && message != "")
+            {
+                byte[] buffer = new byte[1024];
+                buffer = Encoding.UTF8.GetBytes(message);
+                Client.Send(buffer);
+            }
+        }
+
+        private void recvMessage()
+        {
+            byte[] buffer = new byte[1024];
+            for (int i = 0; i < buffer.Length; i++)
+                buffer[i] = 0;
+
+            for (; ; )
+            {
+                try
+                {
+                    Client.Receive(buffer);
+                    string message = Encoding.UTF8.GetString(buffer);
+                    int count = message.IndexOf(";;;5");
+                    if (count == -1)
+                        continue;
+
+                    string clear_message = "";
+
+                    for (int i = 0; i < count; i++)
+                        clear_message += message[i];
+
+                    this.Invoke((MethodInvoker)delegate ()
+                    {
+                        richTextBox1.AppendText(clear_message);
+                    });
+                }
+                catch (Exception ex) { }
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)  // Кнопка на ввод IP - address и port
+        {
+            // T_b2 - ip, T_b3 - port
+            if (textBox2.Text != "" && textBox3.Text != "")
+            {
+                ip = textBox2.Text;
+                port = Convert.ToInt32(textBox3.Text);
+
+                Client = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+                if (ip != null)
+                {
+                    Client.Connect(ip, port);
+                    th_add = new Thread(delegate () { recvMessage(); });
+
+                    textBox2.Enabled = false;
+                    textBox3.Enabled = false;
+                    button3.Enabled = false;
+
+                    textBox1.Enabled = true;
+                    button1.Enabled = true;
+                }
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            if(th_add != null)
+                th_add.Abort();
+            if (Client != null)
+                Client.Close();
+            Application.Exit();
         }
     }
 }
--- Компьютерные сети/Лаба3/SERVER/SERVER/SERVER.cpp
+++ Компьютерные сети/Лаба3/SERVER/SERVER/SERVER.cpp
@@ -1,7 +1,73 @@
-﻿#include <iostream>
+﻿#pragma comment(lib,"Ws2_32.lib")
+#include <iostream>
+#include <WinSock2.h>
+#include <WS2tcpip.h>
+
+SOCKET Connect;
+SOCKET* Connections;
+SOCKET Listen;
+
+int ClientCount = 0;
+
+void SendMessageToClient(int ID)
+{
+   char* buffer = new char[1024];
+   for (;; Sleep(75))
+   {
+      memset(buffer, 0, sizeof(buffer));
+      if (recv(Connections[ID], buffer, 1024, NULL))
+      {
+         std::cout << (buffer) << std::endl;
+         for (int i = 0; i <= ClientCount; i++)
+            send(Connections[i], buffer, strlen(buffer), NULL);
+      }
+   }
+   delete(buffer);
+}
 
 int main()
 {
-    std::cout << "Hello World!\n";
+   setlocale(LC_ALL, "rus");
+   WSADATA data;
+   WORD version = MAKEWORD(2, 2);
+   int res = WSAStartup(version, &data);
+   
+   if (res != 0)
+      return 0;
+
+   struct addrinfo hints;
+   struct addrinfo* result;
+
+   Connections = new SOCKET[64];
+
+   ZeroMemory(&hints, sizeof(hints));
+   hints.ai_family = AF_INET;
+   hints.ai_flags = AI_PASSIVE;
+   hints.ai_socktype = SOCK_STREAM;
+   hints.ai_protocol = IPPROTO_TCP;
+
+   getaddrinfo(NULL, "7111", &hints, &result);
+
+   Listen = socket(result->ai_family, result->ai_socktype, result->ai_protocol);
+   bind(Listen, result->ai_addr, result->ai_addrlen);
+   listen(Listen, SOMAXCONN);
+
+   freeaddrinfo(result);
+
+   std::cout << "Start server..." << std::endl;
+   char m_connect[] = "Connect...;;;5";
+
+   for (;; Sleep(75))
+   {
+      if (Connect = accept(Listen, NULL, NULL))
+      {
+         printf("Client connected...\n");
+         Connections[ClientCount] = Connect;
+         send(Connections[ClientCount], m_connect, strlen(m_connect), NULL);
+         CreateThread(NULL, NULL, (LPTHREAD_START_ROUTINE)SendMessageToClient, (LPVOID)(ClientCount - 1), NULL, NULL);
+      }
+   }
+
+   return 1;
 }
 
